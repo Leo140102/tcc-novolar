830b3c4b7e4938a2820f4c46aa03df8ee94d7fff
LProdigY <leowfsouza@gmail.com>
2024-06-05 20:56:42 -0300

    segundo comitt

--- tcc-novolar/server/index.js
+++ tcc-novolar/server/index.js
@@ -3,8 +3,6 @@ const app = express();
 const port = 3000;
 const mysql = require("mysql")
 
-app.use(express.json());
-
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -12,29 +10,57 @@ const db = mysql.createConnection({
     database: "mydb",
 });
 
-app.get("/",(req, res)=>{
-    console.log("Testandando rota insert")
-    db.query("INSERT INTO mydb.locador (cpf, telefone, nome, dtNascimento, idImovelAtual, idImovelAnterior, email, senha,imovel_id) VALUES ('09560828908', '(44)99886466', 'Bruno Xavier', '22/07/1998', 'NULL','NULL','brunoXavier@gmail.com','11223344',1)"),(err,result) =>{     
-        if(err){
-            console.log(err)
-        }       
-    }
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", (req,res)=>{
+    const email = req.body.email
+    const password = req.body.password
+
+    db.query("SELECT * FROM mydb.locador WHERE EMAIL = ?",
+    [email],
+        (err,result) =>{
+            if(err){
+                res.send(err)
+            }
+            if(result.lenght == 0){
+                db.query("INSERT INTO mydb.locatario (email, password) VALUES (?,?)", 
+                [email,password],(err, result) =>{
+                    if(err){
+                        res.send(err)
+                    }
+                    res.send({msg: "Cadastrado com Sucesso"})
+                })
+            }
+            res.send(result)
+        }
+    )
 })
 
-app.get('/', async (req, res) => {
-    try {
-        const db = await connectDB();
-        const result = await db.query("INSERT INTO mydb.locador (cpf, telefone, nome, dtNascimento, idImovelAtual, idImovelAnterior, email, senha, imovel_id) VALUES (?,?,?,?,?,?,?,?,?)", ['09560828908', '4499886466', 'Bruno Xavier', '1998-07-22', null, null, 'brunoXavier@gmail.com', '11223344', 1]);
-        console.log(result);
-        res.send('Dados inseridos com sucesso!');
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Erro ao inserir dados.');
-    } finally {
-        // Fechar a conexão com o banco de dados
-        await db.end();
-    }
-});
+// app.get("/",(req, res)=>{
+//     console.log("Testandando rota insert")
+//     db.query("INSERT INTO mydb.locador (cpf, telefone, nome, dtNascimento, idImovelAtual, idImovelAnterior, email, senha,imovel_id) VALUES ('09560828908', '(44)99886466', 'Bruno Xavier', '22/07/1998', 'NULL','NULL','brunoXavier@gmail.com','11223344',1)"),(err,result) =>{     
+//         if(err){
+//             console.log(err)
+//         }       
+//     }
+// })
+
+// app.get('/', async (req, res) => {
+//     try {
+//         const db = await connectDB();
+//         const result = await db.query("INSERT INTO mydb.locador (cpf, telefone, nome, dtNascimento, idImovelAtual, idImovelAnterior, email, senha, imovel_id) VALUES (?,?,?,?,?,?,?,?,?)", ['09560828908', '4499886466', 'Bruno Xavier', '1998-07-22', null, null, 'brunoXavier@gmail.com', '11223344', 1]);
+//         console.log(result);
+//         res.send('Dados inseridos com sucesso!');
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send('Erro ao inserir dados.');
+//     } finally {
+//         // Fechar a conexão com o banco de dados
+//         await db.end();
+//     }
+// });
 
 app.listen(3000, () =>{
     console.log("Rodando na porta 3000")
