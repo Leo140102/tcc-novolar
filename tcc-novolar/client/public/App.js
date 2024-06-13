808b6670d5f53627ad4d01f549ea883c031e7934
kaamyxx <kamilyxavierp@gmail.com>
2024-06-13 20:18:53 -0300

    teste

--- tcc-novolar/client/public/App.js
+++ tcc-novolar/client/public/App.js
@@ -5,6 +5,26 @@ const container = document.querySelector(".container");
 const login_btn2 = document.querySelector("#login-btn2");
 const cadastro_btn2 = document.querySelector("#cadastro-btn2");
 
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value === "") {
+    parent.classList.remove("focus");
+  }
+
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
 cadastro_btn.addEventListener("click", () => {
   container.classList.add("cadastro-mode");
 });
