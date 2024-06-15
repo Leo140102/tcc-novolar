2dc9073b40ffefca93f1ed83bb6520c076d0549d
LProdigY <leowfsouza@gmail.com>
2024-06-14 22:08:29 -0300

    ajuste porta

--- tcc-novolar/server/index.js
+++ tcc-novolar/server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000;
+const port = 8000;
 const mysql = require("mysql")
 const bcrypt = require("bcrypt")
 const cors = require("cors")
