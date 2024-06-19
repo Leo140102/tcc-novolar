a7a946d38e08187828115bf0d85c51d61b0fdc9d
Leo140102 <leowfsouza@gmail.com>
2024-06-19 17:32:05 -0300

    Cadastro v5fim

--- tcc-novolar/client/public/cadastro.js
+++ tcc-novolar/client/public/cadastro.js
@@ -1,89 +1,87 @@
 console.log("CHAMANDO TELA DE CADASTRO.JS")
 
-function validaCPF(cpf) {
-    let cpfLimpo = cpf.replace(/\D/g, "");
+function TestaCPF(strCPF) {
+    var Soma;
+    var Resto;
+    Soma = 0;
+  if (strCPF == "00000000000") return false;
 
-    if (cpfLimpo.length!== 11) {
-        return false;
-    }
+  for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+  Resto = (Soma * 10) % 11;
 
-    let soma = 0;
-    let resto;
-    for (let i = 1; i <= 9; i++) {
-        soma += parseInt(cpfLimpo.substring(i - 1, i)) * (11 - i);
-    }
-    resto = soma % 11;
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
 
-    if (((resto < 2) && resto!== parseInt(cpfLimpo.substring(9, 10))) || ((resto >= 2) && (resto - 11)!== parseInt(cpfLimpo.substring(9, 10)))) {
-        return false;
-    }
+  Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
 
-    soma = 0;
-    for (let i = 1; i <= 10; i++) {
-        soma += parseInt(cpfLimpo.substring(i - 1, i)) * (12 - i);
-    }
-    resto = soma % 11;
-
-    if (((resto < 2) && resto!== parseInt(cpfLimpo.substring(10, 11))) || ((resto >= 2) && (resto - 11)!== parseInt(cpfLimpo.substring(10, 11)))) {
-        return false;
-    }
-
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
     return true;
 }
 
-function cadastroUser(){
-   
-    
-    document.getElementById('cadastro').addEventListener('submit', function(event) {
+function cadastroUser() {
+
+
+    document.getElementById('cadastro').addEventListener('submit', function (event) {
         event.preventDefault();
-    
+
         const nome = document.getElementById('nome').value.toString().trim();
         const email = document.getElementById('emailCadastro').value.toString().trim();
         const cpf = document.getElementById('cpf').value.toString().trim();
+
+        const cpfValido = cpf.replace(/\D/g, "");
+
+        console.log("CPFLIMPO" + cpfValido)
+
+
+
         const senha = document.getElementById('senhaCadastro').value.toString().trim();
-    
-        if (!nome ||!email ||!cpf ||!senha) {
+
+        
+
+        if (!nome || !email || !cpf || !senha) {
             alert('Todos os campos são obrigatórios.');
         }
-    
+
         const emailValido = /\S+@\S+\.\S+/.test(email);
         if (!emailValido) {
             alert('Por favor, insira um email válido.');
-            return; 
+            return;
         }
-    
-        // const cpfValido = /^\d{11}$/.test(cpf); 
-        // if (!cpfValido) {
-        //     if(!validaCPF(cpfValido.toString())){
-        //         alert('CPF inválidoooooooooooo.');
-        //     }    
-        //     return; 
-        // }
-    
-    
-        const senhaValida = senha.length > 6;
+
+       
+
+        if (!TestaCPF(cpfValido.toString())) {
+            alert('CPF inválidoooooooooooo.');
+            return;
+        }
+
+
+        const senhaValida = senha.length >= 6;
         if (!senhaValida) {
             alert('A senha deve ter mais de 6 caracteres.');
-            return; 
+            return;
         }
-    
-         const senhaSegura = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/.test(senha);
+
+        const senhaSegura = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/.test(senha);
         if (!senhaSegura) {
             alert('A senha deve conter letras maiúsculas, minúsculas e números.');
-            return; 
+            return;
         }
-    
+
         const formCad = {
             nome,
             email,
             cpf,
             senha
         };
-    
+
         const jsonData = JSON.stringify(formCad);
-    
+
         console.log(jsonData)
-    
+
         const options = {
             method: 'POST',
             headers: {
@@ -91,13 +89,13 @@ function cadastroUser(){
             },
             body: jsonData
         };
-    
+
         console.log(options)
-    
+
         fetch('https://seusite.com/api/cadastro', options)
-           .then(response => response.json())
-           .then(data => console.log(data))
-           .catch(error => console.error('Erro:', error));
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error('Erro:', error));
     });
 }
 
