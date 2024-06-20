fa2226339d0627d189d46aff94ba415889c101a2
Leo140102 <leowfsouza@gmail.com>
2024-06-20 10:25:15 -0300

    comit session

--- tcc-novolar/server/index.js
+++ tcc-novolar/server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express()
+
 const PORT = 8000;
 const mysql = require("mysql");
 const bcrypt = require("bcrypt")
@@ -7,13 +8,11 @@ const cors = require("cors")
 const saltRounds = 10
 const bodyParser = require("body-parser")
 const session = require("express-session")
+const flash = require("connect-flash")
 
-const http = require('http')
 const path = require('path')
 
-const fs = require("fs")
-
-
+//Configurações do banco
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -21,20 +20,27 @@ const db = mysql.createConnection({
     database: "mydb",
 });
 
-
 app.use(express.json());
 app.use(cors());
+//Configurações da session
+app.use(session({
+    secret: 'rwZcr7FK', // Uma string secreta usada para assinar o cookie da sessão
+    resave: true, // Força a sessão a ser salva, mesmo se modificada
+    saveUninitialized: true, // Salva sessões não modificadas
+    cookie: { secure: false } // Ativa o atributo Secure do cookie, recomendado para produção
+}));
+app.use(flash())
 
-// app.use(bodyParser.json())
-// app.use(express.urlencoded({ extended: true }))
-// app.use(session({
-//     secret: "secret",
-//     resave: false,
-//     saveUninitialized: true
-// }))
+//Middleware
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+})
+
+/* -------------------- cadastro ----------------*/
 
 app.post("/register", (req, res) => {
-    
+
     const nome = req.body.nome
     const email = req.body.email
     const cpf = req.body.cpf
@@ -44,7 +50,7 @@ app.post("/register", (req, res) => {
         [email],
         (err, result) => {
             if (err) {
-                res.send(err)
+                return res.status(500).send({ error: err });
             }
             console.log(result.length)
             if (result.length === 0) {
@@ -53,15 +59,15 @@ app.post("/register", (req, res) => {
                         "INSERT INTO mydb.locatario (email, senha, cpf, nome) VALUES (?,?,?,?)",
                         [email, hash, cpf, nome], (err, response) => {
                             if (err) {
-                                return  res.send(err)
+                                return res.send(err)
                             }
-                            return  res.send({ msg: "Cadastrado com Sucesso" })
+                            return res.status(201).send('Usuário cadastrado com sucesso!')
                         })
                 })
 
             } else {
                 //res.send({ msg: "Usuário já cadastrado!" })
-                return  res.send({ msg: "Usuário já cadastrado!" })
+                return res.status(409).send('Usuário já cadastrado.');
             }
         }
     )
@@ -82,57 +88,34 @@ app.use("/public/*", (req, res, next) => {
 app.post("/login", (req, res) => {
     const email = req.body.email
     const senha = req.body.senha
-    db.query(      
+    db.query(
         "SELECT * FROM mydb.locatario WHERE email = ?", [email], (err, result) => {
             if (err) {
-                return req.send(err)
+                return res.status(500).send({ error: err });
             }
             console.log(result)
             if (result.length > 0) {
                 bcrypt.compare(senha, result[0].senha,
                     (erro, result) => {
                         if (result) {
-                            console.log("logado")
-                            return res.send("Usuário logado com sucesso!")
-                           
+                            req.session.user = {
+                                nome: result[0].nome,
+                                email: email
+                            };
+                            return res.status(200).send('Login realizado com sucesso.');
+
                         } else {
-                            console.log("senha incorreta")
-                            return res.send("Senha incorreta!")
-                            
+                            return res.status(401).send('Senha incorreta.');
+
                         }
                     })
 
             } else {
-                console.log("ENTRA ELSE");
-                return res.send("Conta não encontrada!")
-                
+                return res.status(404).send('Conta não encontrada.');
+
             }
         })
 })
-
-app.post('/login', async (req, res) => {
-    console.log("INDEX.JS - Rota de Login");
-    const { email, password } = req.body;
-
-    try {
-        const user = await db.query("SELECT * FROM mydb.locatario WHERE email =?", [email]);
-        if (user.length === 0) {
-            return res.status(404).json({ msg: "Conta não encontrada!" });
-        }
-
-        // Comparação direta da senha, não recomendada por motivos de segurança
-        if (password!== user[0].password) {
-            return res.status(401).json({ msg: "Senha incorreta!" });
-        }
-
-        // Aqui você pode iniciar a sessão ou gerar um token JWT, dependendo da sua implementação
-        req.session.loggedin = true;
-        res.status(200).json({ msg: "Usuário logado com sucesso!" });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(error.message);
-    }
-});
 
 /* -------------------- ROTAS ANUNCIOS IMOVEIS ----------------*/
 /* TESTE */
@@ -205,38 +188,6 @@ app.get("/bemAvaliados", (req, res) => {
     })
 })
 
-// function getRegistroById(id, callback) {
-//     let query = 'SELECT * FROM mydb.imovel';
-//     db.query(query, [id], (error, results) => {
-//       if (error) throw error;
-//       callback(null, results[0]);
-//     });
-//   }
-
-//   // Rota GET para obter um registro pelo ID
-//   app.get('/obterImovel/:id', (req, res) => {
-//     const id = req.params.id;
-//     getRegistroById(id, (err, registro) => {
-//       if (err) return res.status(500).send(err.message);
-//       if (!registro) return res.status(404).send('Registro não encontrado.');
-//       res.json(registro);
-//     });
-//   });
-
-
-
-// app.get("/imovel/:id", (req, res) => {
-//     //res.send("Olá Mundo!");
-//     let id = req.params.id;
-//     db.query("SELECT * FROM imovel WHERE id= ?", [req.params.id], (err, results) => {
-//         if (err) {
-//             res.send('err.message');
-//             //res.send("Olá Mundo!");
-//         }
-//         res.json(results);
-//     })
-// });
-
 /* IMOVEIS POR ID */
 app.get("/imoveis/:id", (req, res) => {
     db.query("SELECT id, titulo FROM imovel WHERE id = ?", [req.params.id], (err, results) => {
@@ -246,32 +197,6 @@ app.get("/imoveis/:id", (req, res) => {
         res.json(results);
     })
 });
-
-
-// app.get("/",(req, res)=>{
-//     console.log("Testandando rota insert")
-//     db.query("INSERT INTO mydb.locador (cpf, telefone, nome, dtNascimento, idImovelAtual, idImovelAnterior, email, senha,imovel_id) VALUES ('09560828908', '(44)99886466', 'Bruno Xavier', '22/07/1998', 'NULL','NULL','brunoXavier@gmail.com','11223344',1)"),(err,result) =>{     
-//         if(err){
-//             console.log(err)
-//         }       
-//     }
-// })
-
-// app.get('/', async (req, res) => {
-//     try {
-//         const db = await connectDB();
-//         const result = await db.query("INSERT INTO mydb.locador (cpf, telefone, nome, dtNascimento, idImovelAtual, idImovelAnterior, email, senha, imovel_id) VALUES (?,?,?,?,?,?,?,?,?)", ['09560828908', '4499886466', 'Bruno Xavier', '1998-07-22', null, null, 'brunoXavier@gmail.com', '11223344', 1]);
-//         console.log(result);
-//         res.send('Dados inseridos com sucesso!');
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).send('Erro ao inserir dados.');
-//     } finally {
-//         // Fechar a conexão com o banco de dados
-//         await db.end();
-//     }
-// });
-
 
 app.listen(PORT, () => {
     console.log(`Aplicação rodando na porta ${PORT}`)
