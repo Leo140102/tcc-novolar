3bb192757e1814824948f2fb63ca8cdf194c1075
kaamyxx <kamilyxavierp@gmail.com>
2024-06-24 22:38:41 -0300

    alert css

--- tcc-novolar/client/public/cadastro.js
+++ tcc-novolar/client/public/cadastro.js
@@ -2,20 +2,20 @@ function TestaCPF(strCPF) {
     var Soma;
     var Resto;
     Soma = 0;
-  if (strCPF == "00000000000") return false;
+    if (strCPF == "00000000000") return false;
 
-  for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
-  Resto = (Soma * 10) % 11;
-
-    if ((Resto == 10) || (Resto == 11))  Resto = 0;
-    if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
-
-  Soma = 0;
-    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    for (i = 1; i <= 9; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
     Resto = (Soma * 10) % 11;
 
-    if ((Resto == 10) || (Resto == 11))  Resto = 0;
-    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+    if ((Resto == 10) || (Resto == 11)) Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10))) return false;
+
+    Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11)) Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11))) return false;
     return true;
 }
 
@@ -110,31 +110,37 @@ function cadastroUser() {
 
         const senha = document.getElementById('senhaCadastro').value.toString().trim();
 
-        if (!nome ||!email ||!cpf ||!senha) {
-            alert('Todos os campos são obrigatórios.');
+        if (!nome || !email || !cpf || !senha) {
+            callAlert('Todos os campos são obrigatórios.');
         }
 
         const emailValido = /\S+@\S+\.\S+/.test(email);
         if (!emailValido) {
-            alert('Por favor, insira um email válido.');
+            callAlert('Por favor, insira um email válido.');
             return;
         }
 
         if (!TestaCPF(cpfValido.toString())) {
-            alert('CPF inválido!');
+            callAlert('CPF inválido!');
             return;
         }
 
         const senhaValida = senha.length >= 6;
         if (!senhaValida) {
-            alert('A senha deve ter mais de 6 caracteres.');
+            callAlert('A senha deve ter mais de 6 caracteres.');
             return;
         }
 
         const senhaSegura = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/.test(senha);
         if (!senhaSegura) {
-            alert('A senha deve conter letras maiúsculas, minúsculas e números.');
+            callAlert('A senha deve conter letras maiúsculas, minúsculas e números.');
             return;
+        }
+
+        var agree = document.getElementById("termosConcordancia").checked;
+        if (!agree) {
+            callAlert("Indique que leu e concorda com os Termos e Condições e a Política de Privacidade.");
+            return false;
         }
 
         const formCad = {
@@ -157,15 +163,15 @@ function cadastroUser() {
         console.log(options);
 
         fetch('http://localhost:8000/register', options)
-          .then(response => {
+            .then(response => {
                 if (response.status === 201) {
-                    redirectToLogin(); 
-                } else if(response.status === 409) {
-                    alert('Usuário já cadastrado.');
+                    redirectToLogin();
+                } else if (response.status === 409) {
+                    callAlert('Usuário já cadastrado.');
                 }
             })
-          .then(data => console.log(data))
-          .catch(error => console.error('Erro:', error));
+            .then(data => console.log(data))
+            .catch(error => console.error('Erro:', error));
     }
 
     // Verifica se o evento já foi adicionado
@@ -175,6 +181,45 @@ function cadastroUser() {
         // Marca o atributo para indicar que o evento foi adicionado
         formElement.setAttribute('data-submit-listener-added', '');
     }
+}
+
+function callAlert(msg) {
+    alert(msg);
+}
+
+window.alert = function (message, timeout = null) {
+    console.log(message);
+    mens = document.querySelector('.text'),
+        toast = document.querySelector(".toast"),
+        closeIcon = document.querySelector(".close"),
+        progress = document.querySelector(".progress");
+
+    let timer1, timer2;
+
+    toast.classList.add("active");
+    progress.classList.add("active");
+
+    mens.textContent = message;
+
+    timer1 = setTimeout(() => {
+        toast.classList.remove("active");
+    }, 5000); //1s = 1000 milliseconds
+
+    timer2 = setTimeout(() => {
+        progress.classList.remove("active");
+    }, 5300);
+
+
+    closeIcon.addEventListener("click", () => {
+        toast.classList.remove("active");
+
+        setTimeout(() => {
+            progress.classList.remove("active");
+        }, 300);
+
+        clearTimeout(timer1);
+        clearTimeout(timer2);
+    });
 }
 
 
--- tcc-novolar/client/public/login.js
+++ tcc-novolar/client/public/login.js
@@ -1,12 +1,3 @@
-function validateForm() {
-    event.preventDefault();
-    var agree = document.getElementById("termosConcordancia").checked;
-    if (!agree) {
-        alert("Por favor, indique que leu e concorda com os Termos e Condições e a Política de Privacidade.");
-        return false;
-    }
-    return true;
-}
 
 function logar() {
     console.log("Entrando...");
@@ -27,20 +18,59 @@ function logar() {
         }),
         headers: { "content-type": "application/json" }
     })
-    .then(async (resp) => {
-        var statusCode = await resp.status;
-        if (statusCode === 200) { 
-            window.location.href = "/index.html";
-        } else if (statusCode === 404) {
-            alert('Conta não encontrada.');
-        }else if(statusCode === 401){
-            alert('Senha incorreta.');
-        } else {
-            alert(`Status desconhecido: ${statusCode}`);
-        }
-    })
-    
-   .catch((error) => {
-        console.error('Error:', error);
+        .then(async (resp) => {
+            var statusCode = await resp.status;
+            if (statusCode === 200) {
+                window.location.href = "/index.html";
+            } else if (statusCode === 404) {
+                callAlert('Conta não encontrada.');
+            } else if (statusCode === 401) {
+                callAlert('Senha incorreta.');
+            } else {
+                callAlert(`Status desconhecido: ${statusCode}`);
+            }
+        })
+
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+function callAlert(msg) {
+    alert(msg);
+}
+
+window.alert = function (message, timeout = null) {
+    console.log(message);
+    mens = document.querySelector('.text'),
+        toast = document.querySelector(".toast"),
+        closeIcon = document.querySelector(".close"),
+        progress = document.querySelector(".progress");
+
+    let timer1, timer2;
+
+    toast.classList.add("active");
+    progress.classList.add("active");
+
+    mens.textContent = message;
+
+    timer1 = setTimeout(() => {
+        toast.classList.remove("active");
+    }, 5000); //1s = 1000 milliseconds
+
+    timer2 = setTimeout(() => {
+        progress.classList.remove("active");
+    }, 5300);
+
+
+    closeIcon.addEventListener("click", () => {
+        toast.classList.remove("active");
+
+        setTimeout(() => {
+            progress.classList.remove("active");
+        }, 300);
+
+        clearTimeout(timer1);
+        clearTimeout(timer2);
     });
 }
