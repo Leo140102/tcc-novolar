bd2e897b707a503f90cbfff79d086edfc43cc5a2
Kamily <kamilyxavierp@gmail.com>
2024-06-27 11:12:31 -0300

    mains

--- tcc-novolar/client/src/main.js
+++ tcc-novolar/client/src/main.js
@@ -7,5 +7,25 @@ menu.onclick = () => {
 
 }
 
+// Defina a variável globalmente
+let minhaVariavel = '';
+
+document.getElementById('sair').addEventListener('click', function () {
+    minhaVariavel = '';
+    document.getElementById('perfil').innerText = minhaVariavel;
+    document.getElementById('userSession').innerText = minhaVariavel;
+});
+
+document.getElementById('login').addEventListener('click', function () {
+    console.log("entrou click");
+    fetch('http://localhost:8000/user/name')
+        .then(response => response.json())
+        .then(data => {
+            minhaVariavel = data.nome;
+            document.getElementById('userSession').innerText = minhaVariavel;
+        })
+        .catch(error => console.error('Erro ao carregar nome do usuário:', error));
+
+});
 
 
