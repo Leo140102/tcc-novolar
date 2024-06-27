f97d18343954df757d123f97ae7ab80c6a079cba
LProdigY <leowfsouza@gmail.com>
2024-06-27 12:54:54 -0300

    fim

--- tcc-novolar/client/public/main.js
+++ tcc-novolar/client/public/main.js
@@ -6,26 +6,3 @@ menu.onclick = () => {
     navbar.classList.toggle('active');
 
 }
-
-// Defina a variável globalmente
-let minhaVariavel = '';
-console.log("AQUI");
-document.getElementById('sair').addEventListener('click', function () {
-    minhaVariavel = '';
-    document.getElementById('perfil').innerText = minhaVariavel;
-    document.getElementById('userSession').innerText = minhaVariavel;
-});
-
-document.getElementById('login').addEventListener('click', function () {
-    console.log("entrou click");
-    fetch('http://localhost:8000/user/name')
-        .then(response => response.json())
-        .then(data => {
-            minhaVariavel = data.nome;
-            document.getElementById('userSession').innerText = minhaVariavel;
-        })
-        .catch(error => console.error('Erro ao carregar nome do usuário:', error));
-
-});
-
-
